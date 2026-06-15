dab82bab1cbee4377f0e5911ebb92a54906bcfd0
lafv <armanyfelix@proton.me>
2026-06-15 14:37:24 -0700

    los postCards ya se muestran, falta la redireccion al post

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,33 +1,33 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { glob, file } from "astro/loaders";
+import { z } from "astro/zod";
 
-const postsCollection = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		published: z.date(),
-		updated: z.date().optional(),
-		draft: z.boolean().optional().default(false),
-		description: z.string().optional().default(""),
-		image: z.string().optional().default(""),
-		tags: z.array(z.string()).optional().default([]),
-		category: z.string().optional().nullable().default(""),
-		
-		pinned: z.boolean().optional().default(false),
-
-		series: z.string().optional(),
-
-		/* For internal use */
-		prevTitle: z.string().default(""),
-		prevSlug: z.string().default(""),
-		nextTitle: z.string().default(""),
-		nextSlug: z.string().default(""),
-	}),
+const posts = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./content/posts" }),
+  schema: z.object({
+    title: z.string(),
+    published: z.date(),
+    updated: z.date().optional(),
+    draft: z.boolean().optional().default(false),
+    description: z.string().optional().default(""),
+    image: z.string().optional().default(""),
+    tags: z.array(z.string()).optional().default([]),
+    category: z.string().optional().nullable().default(""),
+    pinned: z.boolean().optional().default(false),
+    series: z.string().optional(),
+    /* For internal use */
+    prevTitle: z.string().default(""),
+    prevSlug: z.string().default(""),
+    nextTitle: z.string().default(""),
+    nextSlug: z.string().default(""),
+  }),
 });
 
-const specCollection = defineCollection({
-	schema: z.object({}),
+const spec = defineCollection({
+  schema: z.object({}),
 });
 
 export const collections = {
-	posts: postsCollection,
-    spec: specCollection,
+  posts,
+  spec,
 };
